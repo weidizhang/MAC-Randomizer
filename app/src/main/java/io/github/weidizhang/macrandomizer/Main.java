78658463f1a53258f2a4a80ca019b6a31b754920
Weidi Zhang <weidiz999@yahoo.com>
2017-01-08 18:36:49 -0800

    Separate handling button clicks into different methods, remove settings menu, restore actual MAC functionality

--- app/src/main/java/io/github/weidizhang/macrandomizer/Main.java
+++ app/src/main/java/io/github/weidizhang/macrandomizer/Main.java
@@ -1,5 +1,7 @@
 package io.github.weidizhang.macrandomizer;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -49,34 +51,47 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.button1) {
-            boolean success = false;
-
-            for (int attempt = 0; attempt < 10; attempt++) {
-                String randomMac = network.generateRandomMac();
-                network.setMacAddress(randomMac);
-
-                updateCurrentMac();
-
-                if (network.getCurrentMac().equals(randomMac)) {
-                    success = true;
-                    Toast.makeText(getApplicationContext(), "Your MAC address has been randomized", Toast.LENGTH_SHORT).show();
-
-                    break;
-                }
-            }
-
-            if (!success) {
-                Toast.makeText(getApplicationContext(), "Unable to randomize your MAC address, try again", Toast.LENGTH_SHORT).show();
-            }
+            handleRandomizeButton();
         }
         else if (v.getId() == R.id.button2) {
-
+            handleRestoreButton();
         }
+    }
+
+    private void handleRandomizeButton() {
+        boolean success = false;
+
+        for (int attempt = 0; attempt < 10; attempt++) {
+            String randomMac = network.generateRandomMac();
+            network.setMacAddress(randomMac);
+
+            updateCurrentMac();
+
+            if (network.getCurrentMac().equals(randomMac)) {
+                success = true;
+                Toast.makeText(getApplicationContext(), "Your MAC address has been randomized", Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+        }
+
+        if (!success) {
+            Toast.makeText(getApplicationContext(), "Unable to randomize your MAC address, try again", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void handleRestoreButton() {
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(false);
+        wifiManager.setWifiEnabled(true);
+
+        updateCurrentMac();
+
+        Toast.makeText(getApplicationContext(), "Your actual MAC address has been restored", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
